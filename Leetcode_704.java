5e57f6aae46ce1679141b0bae81c3f049be2feb5
Simanta-kalita <58041419+Simanta-kalita@users.noreply.github.com>
2023-06-10 15:18:16 +0530

    Create Leetcode_704.java

---
+++ Leetcode_704.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int search(int[] nums, int target) {
+        if(nums.length == 0) return -1;
+        
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(target > nums[mid]){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
